f82dabd1ab7531878b04dfdb01563e6674df7434
bhuvanasrini <projectdealpublish@gmail.com>
2017-12-16 21:32:02 +0530

    get call for tournament and view update

---
+++ src/app/model/tournament.ts
@@ -0,0 +1,9 @@
+export class Tournament {
+    id:number;
+    name:string;
+    category:string;
+    city:string;
+    type:string;
+    customField1:string;
+    customField2:string
+}
--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdminService } from '../service/admin.service';
+import { Tournament } from '../model/tournament';
+
+import 'rxjs/Rx';
+
 @Component({
   selector: 'app-tournament',
   templateUrl: './tournament.component.html',
   styleUrls: ['./tournament.component.css']
 })
 export class TournamentComponent implements OnInit {
-
+  trounnament:any=[];
   constructor(private adminService: AdminService, private router: Router) { }
 
   ngOnInit() {
@@ -18,8 +22,17 @@ export class TournamentComponent implements OnInit {
   getTournament() {
     this.adminService.getTournament().subscribe(
       (respose) => {
-        debugger;
-        console.log(respose )
+        respose.forEach(element => {
+         const tour = new Tournament();
+        tour.id=element.id;
+        tour.name=element.tournament_name;
+        tour.category=element.category;
+        tour.customField2=element.custom_field_value_2;
+        tour.customField1=element.custom_field_value_1;
+        tour.type=element.tournament_type;
+        tour.city=element.city;
+        this.trounnament.push(tour);
+        });
       },
       (error) => {
         console.log(error.json());
